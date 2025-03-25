c1d8b917c90685f03a8d91267e0c0f78527e9818
garvitrai25 <garvitrai25@gmail.com>
2025-03-25 18:45:22 +0530

    Simplify Vercel configuration

--- vercel_app.py
+++ vercel_app.py
@@ -1,11 +1,11 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 # Initialize Django WSGI application
-app = get_wsgi_application()
+app = application = get_wsgi_application()
 
 # Init Vercel serverless function
 application = app 
