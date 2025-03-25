3d139791a19f964c924d2041d6838af01d0e5d7b
garvitrai25 <garvitrai25@gmail.com>
2025-03-25 18:24:03 +0530

    Update Vercel configuration and build settings

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo "Installing dependencies..."
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+
+echo "Making migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear 
