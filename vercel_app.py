ab4e1fbfed9b640cfe0b5cda9c57a8e797d5d9cb
garvitrai25 <garvitrai25@gmail.com>
2025-03-25 18:30:45 +0530

    Update Vercel configuration with direct build commands

--- vercel_app.py
+++ vercel_app.py
@@ -1,4 +1,12 @@
-from core.wsgi import application
+import os
+
+from django.conf import settings
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Initialize Django WSGI application
+application = get_wsgi_application()
 
 # Init Vercel serverless function
 app = application 
